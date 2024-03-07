4713296adc08a5da46def6fad633ebba2f5c03e4
jasonli35 <lijaosn@gmail.com>
2024-03-06 20:43:20 -0800

    add include

--- Archive.hpp
+++ Archive.hpp
@@ -22,6 +22,7 @@
 #include <filesystem>
 #include <algorithm>
 #include <unordered_set>
+#include <cstring>
 namespace ECE141 {
 
     enum class ActionType {added, extracted, removed, listed, dumped, compacted};
