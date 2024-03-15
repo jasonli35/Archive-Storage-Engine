365f063c7d00b51e57238bb34b69c2ca10210c59
jasonli35 <lijaosn@gmail.com>
2024-03-14 20:53:58 -0700

    add idataprocessor

--- IDataProcessor.hpp
+++ IDataProcessor.hpp
@@ -2,7 +2,15 @@
 // Created by Jason Li on 2024/3/13.
 //
 
+#include <vector>
+
 #ifndef ECE141_ARCHIVE_IDATAPROCESSOR_H
 #define ECE141_ARCHIVE_IDATAPROCESSOR_H
 
 #endif //ECE141_ARCHIVE_IDATAPROCESSOR_H
+class IDataProcessor {
+public:
+    virtual std::vector<uint8_t> process(const std::vector<uint8_t>& input) = 0;
+    virtual std::vector<uint8_t> reverseProcess(const std::vector<uint8_t>& input) = 0;
+    virtual ~IDataProcessor(){};
+};
